9c78c0b8acc64fc7dbe706b84efd7062f03c036a
Ekimin <heyiminwork@gmail.com>
2017-01-03 19:29:19 +0800

    添加Date类，中裁同步开发。

--- src/main/java/com/amarsoft/sync/chinajudicial/SyncMain.java
+++ src/main/java/com/amarsoft/sync/chinajudicial/SyncMain.java
@@ -1,8 +1,13 @@
 package com.amarsoft.sync.chinajudicial;
 
 import com.amarsoft.are.ARE;
+import com.amarsoft.dao.chinajudicial.DataManager;
 import com.amarsoft.model.chinajudicial.DataModel;
+import com.amarsoft.util.bloomfilter.BloomFilterFactory;
+import com.amarsoft.util.bloomfilter.BloomFilterManager;
+import com.amarsoft.util.common.DateManager;
 import com.amarsoft.util.hbase.HBaseManager;
+import javafx.scene.effect.Bloom;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -14,75 +19,46 @@ import java.util.List;
  * SyncData
  */
 public class SyncMain {
+
     /**
-     * 同步生产库
+     * 中国裁判文书网数据同步主入口
      */
-    public void syncDataBase(List<DataModel> dataModelList, HBaseManager hBaseControl) {
-        //建立到生产库的数据库连接
-        Connection dbConn = null;
-        PreparedStatement ps = null;
+    public void syncDataMain(){
+        ARE.init();
 
-        String sql = "insert into COURTBULLETIN(SERIALNO,PTYPE,COURT,PARTY,PDATE,PDESC,DATASOURCE,CASENO,DEPARTMENT,CASEDATE,PLAINTIFF,AGENT,SECRETARY,CHIEFJUDGE,JUDGE,NOTICEADDR,DOCUCLASS,TARGET,TARGETTYPE,TARGETAMOUNT,TELNO,PROVINCE,CITY,CASEREASON,COLLECTIONDATE,DEALDATE,DEALPERSON,HTMLFILEPATH,QDATE,DOCUMENTCLASS,CASELEVEL,CARDNO,IPNAME,BATCHNO,CRAWLERID) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        while(true){
+            final int BATCH_SIZE = Integer.parseInt(ARE.getProperty("BATCH_SIZE"));
+            final String DATABASE = ARE.getProperty("SYNC_DATABASE");
+            final String MIN_DATE = ARE.getProperty("MIN_DATE");
+            final String MAX_DATE = ARE.getProperty("MAX_DATE");
+            final String SOLRHOST = ARE.getProperty("SOLRHOST");
 
-        try {
-            dbConn = ARE.getDBConnection("destDB");
-            ps = dbConn.prepareStatement(sql);
-            dbConn.setAutoCommit(false);
-            for (DataModel dataModel : dataModelList) {
-                ps.setString(1, dataModel.getSerialNo());
-                ps.setString(2, dataModel.getpType());
-                ps.setString(3, dataModel.getCourt());
-                ps.setString(4, dataModel.getParty());
-                ps.setString(5, dataModel.getpDate());
-                ps.setString(6, dataModel.getpDesc());
-                ps.setString(7, dataModel.getDataSource());
-                ps.setString(8, dataModel.getCaseNo());
-                ps.setString(9, dataModel.getDepartment());
-                ps.setString(10, dataModel.getCaseDate());
-                ps.setString(11, dataModel.getPlaintiff());
-                ps.setString(12, dataModel.getAgent());
-                ps.setString(13, dataModel.getSecretary());
-                ps.setString(14, dataModel.getChiefJudge());
-                ps.setString(15, dataModel.getJudge());
-                ps.setString(16, dataModel.getNoticeAddress());
-                ps.setString(17, dataModel.getDocuClass());
-                ps.setString(18, dataModel.getTarget());
-                ps.setString(19, dataModel.getTargetType());
-                ps.setString(20, dataModel.getTargetAmount());
-                ps.setString(21, dataModel.getTelNo());
-                ps.setString(22, dataModel.getProvince());
-                ps.setString(23, dataModel.getCity());
-                ps.setString(24, dataModel.getCaseReason());
-                ps.setString(25, dataModel.getCollectionDate());
-                ps.setString(26, dataModel.getDealDate());
-                ps.setString(27, dataModel.getDealPerson());
-                ps.setString(28, dataModel.getHTMLFilePath());
-                ps.setString(29, dataModel.getqDate());
-                ps.setString(30, dataModel.getDocumentClass());
-                ps.setString(31, dataModel.getCaseLevel());
-                ps.setString(32, dataModel.getCardNo());
-                ps.setString(33, dataModel.getIPName());
-                ps.setString(34, dataModel.getBatchNo());
-                ps.setString(35, dataModel.getCrawlerID());
+            final String ZK_HOST = ARE.getProperty("ZK_HOST");
+            final String ZK_PORT = ARE.getProperty("ZK_PORT");
+            final String HTABLE = ARE.getProperty("HBASE_TABLE");
+            final String HFAMILY = ARE.getProperty("HBASE_FAMILY");
+            final String QUALIFIER = ARE.getProperty("QUALIFIER");
 
-                ps.addBatch();
-            }
-            ps.executeBatch();
-            dbConn.commit();
-        } catch (SQLException e) {
-            ARE.getLog().error("同步生产数据库出错", e);
-            e.printStackTrace();
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (dbConn != null) {
-                    dbConn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            ARE.getLog().info("=======================================================");
+            ARE.getLog().info("本次同步数据库：" + DATABASE);
+            ARE.getLog().info("同步时间区间：" + MIN_DATE + " TO " + MAX_DATE);
+            ARE.getLog().info("同步solr地址：" + SOLRHOST);
+            ARE.getLog().info("同步HBase表名：" + HTABLE + "; 列簇：" + HFAMILY);
+            ARE.getLog().info("BATCH_SIZE：" + BATCH_SIZE);
+            ARE.getLog().info("=======================================================");
+
+            //初始化布隆过滤器
+            BloomImpl bloomImpl = new BloomImpl();
+            ARE.getLog().info("初始化布隆过滤器>>>>>>>>>>");
+            bloomImpl.init();
+            ARE.getLog().info("初始化布隆过滤器结束<<<<<<");
+
+            DataManager dataManager = new DataManager();
+            //取数
+            ARE.getLog().info("开始从数据库获取需要同步的数据>>>>>>>>");
+            dataManager.getDataNumByDateRange(DATABASE, DateManager.getCurrentDate());
         }
     }
+
+
 }
