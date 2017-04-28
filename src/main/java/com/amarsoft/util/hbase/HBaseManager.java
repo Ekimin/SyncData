7d01263bd81e85aaae57a1719ea42d786371ecf2
Ekimin <heyiminwork@gmail.com>
2017-04-28 16:35:40 +0800

    mod hbasemanager

--- src/main/java/com/amarsoft/util/hbase/HBaseManager.java
+++ src/main/java/com/amarsoft/util/hbase/HBaseManager.java
@@ -17,7 +17,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by ymhe on 2016/12/28.
+ * Created by Ekimin on 2016/12/28.
  * ChinaJudicialDataTransfer
  */
 public class HBaseManager {
